a3a850a4c239a060857ebf1c4f7f8d303dbaa460
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-10 23:05:02 +0900

    [level 0] Title: 배열의 평균값, Time: 0.06 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,4 +1,6 @@
 import statistics
+import numpy 
 def solution(numbers):
-    # return sum(numbers)/len(numbers) 첫번째 방법 
-    return statistics.mean(numbers) # 두번째 방법 
+    # return sum(numbers)/len(numbers) 첫번째 방법
+    # return statistics.mean(numbers) 두번째 방법
+    return numpy.mean(numbers) # 세번째 방법 
