05816e308c73e2b4c703d802022ddfe584921f20
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-10 23:03:43 +0900

    Update 배열의 평균값.py

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.py
@@ -1,3 +1,4 @@
-import numpy
+import statistics
 def solution(numbers):
-    return sum(numbers)/len(numbers)
+    # return sum(numbers)/len(numbers) 첫번째 방법 
+    return statistics.mean(numbers) # 두번째 방법 
