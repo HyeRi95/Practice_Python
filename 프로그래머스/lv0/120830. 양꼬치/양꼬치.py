9c9633f1a8d725bdd7392e08a3daf04822ffd39c
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-14 12:24:26 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
@@ -1,6 +1,9 @@
-def solution(n, k):
-    answer = 0
-    answer = n*12000 + k*2000
-    if n >= 10 :
-        answer = n*12000 + (k-(n//10))*2000    
-    return answer
+# def solution(n, k):
+#     answer = 0
+#     answer = n*12000 + k*2000
+#     if n >= 10 :
+#         answer = n*12000 + (k-(n//10))*2000    
+#     return answer
+
+def solution(n,k):
+    return  n*12000 + (k-(n//10))*2000
