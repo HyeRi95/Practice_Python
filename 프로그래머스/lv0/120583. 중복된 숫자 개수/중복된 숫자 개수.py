c36a13432b197a9484514b7ef7552e537ce01159
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-15 22:27:32 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,6 +1,9 @@
-def solution(array, n):
-    answer =[]
-    for i in array:
-        if i == n : 
-            answer.append(i)
-    return len(answer)
+# def solution(array, n):
+#     answer =[]
+#     for i in array:
+#         if i == n : 
+#             answer.append(i)
+#     return len(answer)
+
+def solution(array,n):
+    return array.count(n)
