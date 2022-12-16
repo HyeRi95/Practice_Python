f15dfa1785214ea91f1e3953a4c4080fb349487c
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-16 12:06:53 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,6 +1,13 @@
+# def solution(array, height):
+#     answer=[]
+#     for i in array :
+#         if i > height :
+#             answer.append(i)
+#     return len(answer)
+
 def solution(array, height):
-    answer=[]
+    answer = 0
     for i in array :
-        if i > height :
-            answer.append(i)
-    return len(answer)
+        if i > height:
+            answer +=1
+    return answer
