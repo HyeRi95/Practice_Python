428212cc3de71ecd6810ba6020c93d78c6639cc2
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-21 15:34:38 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.00 ms, Memory: 9.92 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,16 +1,19 @@
-def solution(s):
-    p_list =[]
-    y_list =[]
-    for i in s :
-        if i == 'p' or i =='P'  :
-            p_list.append(i)
-        if i == 'y' or i == 'Y':
-            y_list.append(i)
-    if len(p_list) == len(y_list) :
-        answer = True
-    if len(p_list) != len(y_list) :
-        answer = False
-    if len(p_list) == 0 and len(y_list) ==0 :
-        answer = True
+# def solution(s):
+#     p_list =[]
+#     y_list =[]
+#     for i in s :
+#         if i == 'p' or i =='P'  :
+#             p_list.append(i)
+#         if i == 'y' or i == 'Y':
+#             y_list.append(i)
+#     if len(p_list) == len(y_list) :
+#         answer = True
+#     if len(p_list) != len(y_list) :
+#         answer = False
+#     if len(p_list) == 0 and len(y_list) ==0 :
+#         answer = True
 
-    return answer
+#     return answer
+
+def solution(s):
+    return s.lower().count('p') == s.lower().count('y')
