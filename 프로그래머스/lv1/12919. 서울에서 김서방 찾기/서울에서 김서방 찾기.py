f014d091a21f98b10dcff3b52402aa191703ba84
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-24 16:51:18 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,8 +1,11 @@
+# def solution(seoul):
+#     answer = ''
+#     number = ''
+#     for i in range(len(seoul)):
+#         if seoul[i] == 'Kim':
+#             return ('김서방은 '+str(i)+'에 있다')
+        
 def solution(seoul):
-    answer = ''
-    number = ''
-    for i in range(len(seoul)):
-        if seoul[i] == 'Kim':
-            return ('김서방은 '+str(i)+'에 있다')
+    return "김서방은 {}에 있다".format(seoul.index('Kim'))
         
             
