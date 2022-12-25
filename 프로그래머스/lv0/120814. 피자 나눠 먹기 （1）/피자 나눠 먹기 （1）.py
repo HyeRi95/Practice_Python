0df5ad0ec2e66c0c0eb50692d720aff1eb23a46d
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-25 23:16:09 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
@@ -1,6 +1,9 @@
+# def solution(n):
+#     answer =0
+#     answer = n//7
+#     if n%7 != 0 :
+#         answer = answer +1
+#     return answer
+
 def solution(n):
-    answer =0
-    answer = n//7
-    if n%7 != 0 :
-        answer = answer +1
-    return answer 
+    return (n - 1) // 7 + 1
