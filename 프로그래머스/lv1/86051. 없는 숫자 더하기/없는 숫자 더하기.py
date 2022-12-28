29d62a9aaee632c247c9f3f0aaa38cf5e2439090
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-28 14:18:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,3 +1,13 @@
+# 0-9까지의 합을 알고있다고 가정 
+def solution(numbers):
+    return 45-sum(numbers)
+
+# 0-9까지 리스트로 만들어 풀기
+def solution(numbers):
+    a = sum([0,1,2,3,4,5,6,7,8,9])
+    return a - sum(numbers)
+
+# for문 사용해서 0-9까지 더하기 
 def solution(numbers):
     a = 0
     for i in range(10):
