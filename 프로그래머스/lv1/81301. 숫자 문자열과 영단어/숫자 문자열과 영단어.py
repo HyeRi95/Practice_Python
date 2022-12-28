9903ce7a4ffc0cdb1d6160cf1b86d8545de6ad74
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2022-12-28 14:24:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,8 +1,18 @@
-def solution(s):
-    english = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5',
-               'six':'6','seven':'7','eight':'8','nine':'9'}
+# 딕셔너리로 풀기 
+# def solution(s):
+#     english = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5',
+#                'six':'6','seven':'7','eight':'8','nine':'9'}
 
-    for key in english:
-        if key in s :
-            s = s.replace(key,english[key])
+#     for key in english:
+#         if key in s :
+#             s = s.replace(key,english[key])
+#     return int(s)
+
+# 리스트로 풀기 
+def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(len(words)):
+        s = s.replace(words[i], str(i))
+
     return int(s)
