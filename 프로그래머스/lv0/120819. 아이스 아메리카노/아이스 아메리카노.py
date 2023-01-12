541684ec9dbd2eab036bc867fa4f853d16d3c814
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-01-12 17:38:58 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -0,0 +1,9 @@
+def solution(money):
+    answer_c = 0
+    for i in range (1000000//5500) : 
+        if money >= 5500 :
+            money = money - 5500
+            answer_c = answer_c + 1 
+        if money < 5500 :
+            break
+    return [answer_c, money]
