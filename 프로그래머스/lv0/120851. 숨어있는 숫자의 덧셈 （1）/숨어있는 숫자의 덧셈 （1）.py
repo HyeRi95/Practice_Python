14b20928df8e383981a72c5880da34ade1d39d23
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-01-29 21:59:11 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,12 +1,21 @@
+# def solution(my_string):
+#     answer = []
+#     string_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+#     my_string = my_string.upper()
+#     for i in my_string : 
+#         if i in string_list :
+#             pass 
+#         else :
+#             answer.append(int(i))    
+#     return sum(answer
+
 def solution(my_string):
-    answer = []
-    string_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    my_string = my_string.upper()
-    for i in my_string : 
-        if i in string_list :
-            pass 
-        else :
-            answer.append(int(i))
-        
-            
-    return sum(answer)
+    answer = 0
+    for i in my_string:
+        try:
+            answer = answer + int(i)
+        except:
+            pass
+
+    return answer
+
