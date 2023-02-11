3632b11cacc14717df779f0ae3982028c5b8bb13
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-02-11 23:18:40 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 9.95 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,11 @@
+# def solution(my_string):
+    # answer = ''
+    # for i in my_string :
+    #     if i.isupper() == True : 
+    #         answer = answer + i.lower()
+    #     elif i.islower() == True:
+    #         answer = answer + i.upper()
+    # return answer
+
+def solution(my_string):
+    return my_string.swapcase()
