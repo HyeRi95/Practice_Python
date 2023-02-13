7a887a7803922ecf359f32c0db1b7782406559ab
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-02-13 17:16:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,7 +1,15 @@
-def solution(rsp):
-    answer = ''
-    dict = {'2' : '0' ,'0' : '5' , '5' : '2'}
-    for i in rsp : 
-        answer =  answer +dict[i]
+# def solution(rsp):
+#     answer = ''
+#     dict = {'2' : '0' ,'0' : '5' , '5' : '2'}
+#     for i in rsp : 
+#         answer =  answer +dict[i]
         
-    return answer
+#     return answer
+def solution(rsp):
+    rsp =rsp.replace('2','s')
+    rsp =rsp.replace('5','p')
+    rsp =rsp.replace('0','r')
+    rsp =rsp.replace('r','5')
+    rsp =rsp.replace('s','0')
+    rsp =rsp.replace('p','2')
+    return rsp
