c6338542b5e90678d3fdbb0b896f37c6833a57ea
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-02-15 23:14:05 +0900

    [level 0] Title: 세균 증식, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.py
@@ -1,5 +1,8 @@
+# def solution(n, t):
+#     answer = 0
+#     for i in range(t):
+#         answer = 2**i + answer
+#     return n*(answer+1)
+
 def solution(n, t):
-    answer = 0
-    for i in range(t):
-        answer = 2**i + answer
-    return n*(answer+1)
+    return n << t
