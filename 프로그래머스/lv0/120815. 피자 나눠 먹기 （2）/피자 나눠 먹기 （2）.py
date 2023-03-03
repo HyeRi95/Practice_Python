a82f168ebc137bf6d3069ced3c3d7b8342d7fbbe
HyeRi95 <104615414+HyeRi95@users.noreply.github.com>
2023-03-03 16:00:05 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,6 +1,13 @@
+# def solution(n):
+#     for i in range (600):
+#         if (6*(i+1))%n == 0:
+#             return i+1 
+#         else : 
+#             pass
+        
 def solution(n):
-    for i in range (600):
-        if (6*(i+1))%n == 0:
-            return i+1 
-        else : 
-            pass
+    i=1
+    while(1):
+        if (6*i)%n==0:
+            return i
+        i+=1
